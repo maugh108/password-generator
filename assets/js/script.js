fea062edd2784845cc0b5ca9a03927c55ad9d556
maugh108 <maugh108@outlook.com>
2022-10-01 19:23:17 -0500

    caracteres especiales actualizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var charnumbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var charlower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charcapital = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var charspec = [ "@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+var charspec = [ "!", "#", "%", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "~", "?", "@", "[", "]", "^", "_", "{", "}", "|", "~"];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -67,22 +67,6 @@ function generatePassword() {
 
   return generatedpassword;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Escriba la contraseña en la entrada #password
 function writePassword() {
